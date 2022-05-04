c43d6a8d66bfebdb6f13ec8ca3656ec260b330c2
kyeongwoo <drd9811@naver.com>
2022-05-04 15:27:50 +0900

    update 220504

---
+++ src/string/String0106.java
@@ -0,0 +1,23 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class String0106 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        String result = "";
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.indexOf(str.charAt(i)) == i){
+                result += str.charAt(i);
+            }
+        }
+        System.out.println(result);
+    }
+}
---
+++ src/string/String0107.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class String0107 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine().toLowerCase();
+
+        StringBuilder result = new StringBuilder();
+
+        for(int i = str.length()-1; i >=0; i--){
+            result.append(str.charAt(i));
+        }
+
+        if(str.equals(result.toString())){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
+    }
+}
