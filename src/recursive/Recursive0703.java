e2eb0931d4cd929c87100177a95a794be0af42d4
kyeongwoo <drd9811@naver.com>
2022-06-08 20:17:27 +0900

    update 220608

--- src/recursive/Recursive0703.java
+++ src/recursive/Recursive0703.java
@@ -1,4 +1,23 @@
 package recursive;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Recursive0703 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(recursive(n));
+    }
+
+    public static int recursive(int n){
+        if(n == 1){
+            return 1;
+        }
+        return n * recursive(n-1);
+    }
 }
