4d703560db0223b7ac624f5cd7e4e20d835165f9
kyeongwoo <drd9811@naver.com>
2022-06-14 15:10:52 +0900

    update 220614

---
+++ src/recursive/Tree0705.java
@@ -0,0 +1,14 @@
+package recursive;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Tree0705 {
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
