bfc61855287836779140261fcc3043d14b00b593
kyeongwoo <drd9811@naver.com>
2022-07-02 14:56:41 +0900

    update 220702

--- src/recursive/TreeDFS0709.java
+++ src/recursive/TreeDFS0709.java
@@ -1,13 +1,31 @@
 package recursive;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class TreeDFS0709 {
 
+    public static int DFS(int level, Node root) {
+        if(root.lt == null && root.rt == null) return level;
+        else return Math.min(DFS(level+1, root.lt), DFS(level+1, root.rt));
+    }
+
     public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Node root = new Node(1); // root
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
 
+        int length = DFS(0, root);
+        System.out.println(length);
+    }
 
+    static class Node {
+        int data;
+        Node lt, rt; // 자식 Node
+
+        public Node(int val) {
+            this.data = val;
+            this.lt = null;
+            this.rt = null;
+        }
     }
 }
