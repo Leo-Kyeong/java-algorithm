9b04cec543b049de640ea245e57c1eb3f4662eb4
kyeongwoo <drd9811@naver.com>
2022-07-26 23:29:50 +0900

    update 220726

--- src/recursive/Graph0714.java
+++ src/recursive/Graph0714.java
@@ -5,6 +5,10 @@ import java.io.InputStreamReader;
 
 public class Graph0714 {
 
+    public static void DFS(int v) {
+
+    }
+
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
