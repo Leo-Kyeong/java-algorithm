92c0286d5a393792f9c6165f294c99b44f7ba1d1
kyeongwoo <drd9811@naver.com>
2022-08-09 20:18:31 +0900

    update 220807

--- src/dfsbfs/Dfs0801.java
+++ src/dfsbfs/Dfs0801.java
@@ -10,21 +10,21 @@ import java.util.StringTokenizer;
  */
 public class Dfs0801 {
 
-    private static String result = "NO";
-    private static int n, total = 0;
-    private static boolean flag = false;
+    private static String result = "NO"; // 결과
+    private static int n, total = 0; // 원소 갯수, 원소의 총 합
+    private static boolean flag = false; // flag 를 통해 복귀하는 재귀 함수들을 모두 리턴 (성능 최적화)
 
     private static void DFS(int level, int sum, int[] arr) {
-        if(flag) return;
-        if(sum > total / 2) return;
-        if (level == n) {
-            if((total - sum) == sum){
+        if(flag) return; // true 일 경우 복귀하는 재귀 함수는 모두 return
+        if(sum > total / 2) return; // 총 합(total) 보다 부분 집합의 합(sum)이 더 커지면 더 탐색할 필요가 없으므로 return
+        if (level == n) { // 현재 레벨과 원소의 개수(n)가 같다면 (하나의 부분 집합이 만들어질 때까지)
+            if((total - sum) == sum){ // 두 부분 집합의 원소의 합이 같은지 확인
                 result = "YES";
                 flag = true;
             }
         } else {
-            DFS(level+1, sum+arr[level], arr);
-            DFS(level+1, sum, arr);
+            DFS(level+1, sum+arr[level], arr); // 원소 포함
+            DFS(level+1, sum, arr); // 원소 미포함
         }
     }
 
