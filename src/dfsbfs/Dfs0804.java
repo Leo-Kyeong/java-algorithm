59f527ce2174d2c9ec1cbe04c976dcff225a24e4
kyeongwoo <drd9811@naver.com>
2022-08-20 14:31:43 +0900

    update 220820

--- src/dfsbfs/Dfs0804.java
+++ src/dfsbfs/Dfs0804.java
@@ -6,22 +6,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Dfs0804 {
+	private static int n, m;
+	private static int[] pm;
 
-    private static int n, m;
-    private static int[] pm;
+	private static void DFS(int level) {
+		if (level == m) {
+			for (int x : pm) System.out.print(x + " ");
+			System.out.print("\n");
+			return;
+		}
+		for (int i = 1; i <= n; i++) {
+			pm[level] = i;
+			DFS(level + 1);
+		}
+	}
 
-    private static void DFS(int level) {
-
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        n = Integer.parseInt(st.nextToken()); // 1부터 N까지 번호
-        m = Integer.parseInt(st.nextToken()); // M번을 뽑아 일렬로 나열하는 방법
-
-        DFS(0);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(st.nextToken()); // 1부터 N까지 번호
+		m = Integer.parseInt(st.nextToken()); // M번을 뽑아 일렬로 나열하는 방법
+		pm = new int[m];
+		DFS(0);
+	}
 }
