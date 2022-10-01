1208efb6de1ddea50ed58be33c2696427c35a03f
kyeongwoo <drd9811@naver.com>
2022-10-01 20:36:44 +0900

    update 221001

--- src/dfsbfs/Dfs0806.java
+++ src/dfsbfs/Dfs0806.java
@@ -25,7 +25,6 @@ public class Dfs0806 {
 			}
 		}
 	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
